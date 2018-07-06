1bdcc76c7ae056403822c5b913910d2759b38611
Tiago Costa <tiagoffcc@hotmail.com>
2018-07-06 02:54:02 +0100

    feat(NA): added runner for webpack. refact(NA): removed configurator.

--- bundler/configurator/index.js
+++
@@ -1,3 +0,0 @@
-const configurator = require('./configurator');
-
-module.exports = configurator;
---
+++ bundler/run-webpack.js
@@ -0,0 +1,23 @@
+const webpack = require("webpack");
+
+module.export = function (config) {
+  webpack(config, (err, stats) => {
+    if (err) {
+      console.error(err.stack || err);
+      if (err.details) {
+        console.error(err.details);
+      }
+      return;
+    }
+
+    const info = stats.toJson();
+
+    if (stats.hasErrors()) {
+      console.error(info.errors);
+    }
+
+    if (stats.hasWarnings()) {
+      console.warn(info.warnings);
+    }
+  });
+};
