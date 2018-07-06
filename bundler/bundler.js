5e84039f874ae092afc6ae7b06d37315b8bf33c7
Tiago Costa <tiagoffcc@hotmail.com>
2018-07-06 02:57:04 +0100

    refact(NA): bundler entry file.

--- bundler/bundler.js
+++ bundler/bundler.js
@@ -1,17 +1,22 @@
 const compiler = require('./compiler');
 const loader = require('./loader');
-const configurator = require('./configurator');
+const compilerConfig = require('./compiler/config');
+const loaderConfig = require('./loader/config');
 const optionsValidator = require('./options-validator');
 const BUNDLER_TYPES = require('./types');
 
 module.exports = (options = {}) => {
   const { mode, type, exportConfig} = optionsValidator(options);
 
-  if (exportConfig) {
-    return configurator({mode, type})
+  console.log(`${exportConfig ? 'Exporting configuration' : 'Starting bundler'} as ${ type } in a ${ mode } environment.`);
+
+  if (exportConfig && type === BUNDLER_TYPES.COMPILER) {
+    return compilerConfig(mode);
   }
 
-  console.log(`Starting bundler as ${ type } in a ${ mode } environment.`);
+  if (exportConfig) {
+    return loaderConfig(mode);
+  }
 
   if (type === BUNDLER_TYPES.COMPILER) {
     return compiler(mode);
