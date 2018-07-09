bf917cf7d80fb9e8e0f7e0d6293f813c47a6010c
Tiago Costa <tiagoffcc@hotmail.com>
2018-07-10 00:40:56 +0100

    feat(NA): include default options for loader and compiler.

--- bundler/compiler/config/default-options.js
+++ bundler/compiler/config/default-options.js
@@ -1,3 +1,14 @@
-{
+const { PATHS } = require('../../constants');
+const PKG_FILE = require(PATHS.pkg);
 
-}
+module.exports = {
+  dllEntries: [
+    {
+      name: 'vendor',
+      include: Object.keys(PKG_FILE.dependencies),
+      exclude: []
+    },
+  ],
+  dllDependencies: [],
+  compilationResultCache: {}
+};
--- bundler/loader/config/default-options.js
+++ bundler/loader/config/default-options.js
@@ -1,3 +1,10 @@
-{
+const { PATHS } = require('../../constants');
+const COMPILATION_RESULT_FILE = require(PATHS.compilationResult);
+const filteredDllNames = Object.keys(COMPILATION_RESULT_FILE)
+  .filter((dll) => /\.dll\.js$/.test(dll))
+  .map((dll) => dll.replace('.dll.js', ''));
 
-}
+module.exports = {
+  dlls: filteredDllNames,
+  compilationResultFile: COMPILATION_RESULT_FILE
+};
